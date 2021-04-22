d5dd75e5a136d465af802f5a330592075b42f1c8
Hanna-Syniavska <annarybalko34@gmail.com>
2021-04-22 14:01:32 +0300

    add configs

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: 'avoid',
+};
