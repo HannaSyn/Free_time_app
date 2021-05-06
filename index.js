a723f96126e64d82232d5a3c29b9aab63b626cf6
Hanna-Syniavska <annarybalko34@gmail.com>
2021-05-06 18:43:59 +0300

    getting data from url

--- index.js
+++ index.js
@@ -1,86 +1,5 @@
-const activities = [
-  {
-    activity: 'Learn a new programming language',
-    accessibility: 0.25,
-    type: 'education',
-    participants: 1,
-    price: 0.1,
-    key: '5881028',
-  },
-  {
-    activity: "Resolve a problem you've been putting off",
-    type: 'busywork',
-    participants: 1,
-    price: 0,
-    link: '',
-    key: '9999999',
-    accessibility: 0,
-  },
-  {
-    activity: 'Go to a concert with some friends',
-    type: 'social',
-    participants: 4,
-    price: 0.6,
-    link: '',
-    key: '4558850',
-    accessibility: 0.4,
-  },
-  {
-    activity: 'Practice coding in your favorite lanaguage',
-    type: 'recreational',
-    participants: 1,
-    price: 0,
-    link: '',
-    key: '7096020',
-    accessibility: 0.1,
-  },
-  {
-    activity: 'Make a simple musical instrument',
-    type: 'music',
-    participants: 1,
-    price: 0.4,
-    link: '',
-    key: '7091374',
-    accessibility: 0.25,
-  },
-  {
-    activity: 'Write a list of things you are grateful for',
-    type: 'relaxation',
-    participants: 1,
-    price: 0,
-    link: '',
-    key: '2062010',
-    accessibility: 0,
-  },
-  {
-    activity: 'Donate blood at a local blood center',
-    type: 'charity',
-    participants: 1,
-    price: 0,
-    link: 'https://www.redcross.org/give-blood',
-    key: '6509779',
-    accessibility: 0.35,
-  },
-  {
-    activity: 'Learn woodworking',
-    type: 'diy',
-    participants: 1,
-    price: 0.3,
-    link: '',
-    key: '9216391',
-    accessibility: 0.3,
-  },
-  {
-    activity: 'Make bread from scratch',
-    type: 'cooking',
-    participants: 1,
-    price: 0.2,
-    link: '',
-    key: '4809815',
-    accessibility: 0.2,
-  },
-];
-
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-console */
 const types = [
   'Education',
   'Recreational',
@@ -92,6 +11,16 @@ const types = [
   'Music',
   'Busywork',
 ];
+
+const url = 'http://www.boredapi.com/api/activity/';
+let data = {};
+
+function getData() {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => ({ data }))
+    .finally(renderApp());
+}
 
 const dataStore = {
   currentActivity: {},
@@ -143,7 +72,7 @@ function getRandomId(min = 0, max = 8) {
 }
 
 function getRandomActivity() {
-  dataStore.currentActivity = activities[getRandomId()];
+  dataStore.currentActivity = data;
   dataStore.currentType = dataStore.currentActivity.type;
 
   return `${dataStore.currentActivity.activity}`;
@@ -153,4 +82,7 @@ function renderApp() {
   document.querySelector('#app').innerHTML = App();
 }
 
-renderApp();
+document.addEventListener('DOMContentLoaded', function () {
+  getData();
+  // renderApp();
+});
