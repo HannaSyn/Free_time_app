2fe92ecf1eb9af1a78f70224c6416bf38a00dbe5
Hanna-Syniavska <annarybalko34@gmail.com>
2021-05-24 21:01:59 +0300

    add new functionality

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ function App() {
     <h1>Hello my friend!</h1>
     <h2>Are you really bored?</h2>
     <p>Let's see what we can do about it</p>
-    <button onClick="window.dataStore.currentType = ''; window.getNewActivity()">Find random</button>
+    <button onClick="window.dataStore.currentType = ''; window.getNewActivity()">Find random activity</button>
     ${setType()}
     <p class="activity">${renderActivity()}</p>
     <p>Are you still bored?</p></p>
-    <button onClick="window.getNewActivity()">Find more</button>
+    <button onClick="window.getNewActivity()">Find more ${window.dataStore.currentType}</button>
   `;
 }
 
