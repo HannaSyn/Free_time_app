1be7089470163ce4f5ecca351813b46362517900
Hanna Syniavska <62648675+HannaSyn@users.noreply.github.com>
2021-06-18 00:20:47 +0400

    delete tolowercase

--- src/data/getData.js
+++ src/data/getData.js
@@ -1,7 +1,7 @@
 const url = 'https://www.boredapi.com/api/activity/';
 
 export default function getData(currentType) {
-  return fetch(currentType ? `${url}?type=${currentType.toLowerCase()}` : url).then(response => {
+  return fetch(currentType ? `${url}?type=${currentType}` : url).then(response => {
     return response.json();
   });
 }
